7c57236fd5188ca5e82f9f150b55f36e695fbe02
loogark <ragoolkrishnan.ram@gmail.com>
2024-06-16 00:12:56 +0200

    fix hide created with notice

--- src/components/HomeComponents.tsx
+++ src/components/HomeComponents.tsx
@@ -2,6 +2,7 @@
 
 import { Box, Flex } from '@chakra-ui/react'
 import { BlockComponents } from './BlockComponents'
+import { CreatedWithNotice } from './CreatedWithNotice'
 import { Hero } from './Hero'
 
 interface Props {
@@ -9,15 +10,16 @@ interface Props {
 }
 
 export const HomeComponents = ({ data }: Props) => {
-	console.log('data', data)
+	console.log(data, 'data')
 	return (
 		<Box>
 			<Box as="section">
-				<Hero project={data?.project?.project} />
+				<Hero project={data?.project} />
 			</Box>
 			<Flex position="relative" justify="center" align="center" my="52px" w="100%" as="section">
 				<BlockComponents data={data?.page} />
 			</Flex>
+			<CreatedWithNotice shouldHide={data?.project?.hideCreatedWithNotice} />
 		</Box>
 	)
 }
